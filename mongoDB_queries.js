8298fa33da85e608ca8486867ae53189b5701bd0
stephanie <stephieplusthree@gmail.com>
2020-09-17 10:16:43 -0700

    test passed

--- mongoDB_queries.js
+++ mongoDB_queries.js
@@ -121,6 +121,8 @@ const { Db, ObjectId } = require("mongodb");
 // query the movies collection to:
 
 // get all documents
+db.movies.find().pretty()
+
 // get all documents with writer set to "Quentin Tarantino"
 // get all documents where actors include "Brad Pitt"
 // get all documents with franchise set to "The Hobbit"
@@ -130,16 +132,31 @@ const { Db, ObjectId } = require("mongodb");
 // add a synopsis to "The Hobbit: An Unexpected Journey" : "A reluctant hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home - and the gold within it - from the dragon Smaug."
 // add a synopsis to "The Hobbit: The Desolation of Smaug" : "The dwarves, along with Bilbo Baggins and Gandalf the Grey, continue their quest to reclaim Erebor, their homeland, from Smaug. Bilbo Baggins is in possession of a mysterious and magical ring."
 // add an actor named "Samuel L. Jackson" to the movie "Pulp Fiction"
-// Text Search
+
+/////////////////////Text Search/////////////////////////////////////
+
 // find all movies that have a synopsis that contains the word "Bilbo"
+db.movies.createIndex({ synopsis : 'text'})
+db.movies.find({ $text: { $search: 'bilbo'}}).pretty()
+
 // find all movies that have a synopsis that contains the word "Gandalf"
+db.movies.find({$text: { $search: 'Gandalf'}}).pretty()
+
 // find all movies that have a synopsis that contains the word "Bilbo" and not the word "Gandalf"
+db.movies.fin({$text: { $search: 'Bilbo -gandalf'}}).pretty()
+
 // find all movies that have a synopsis that contains the word "dwarves" or "hobbit"
+db.movies.find({ $text: { $search: 'dwarves hobbit'}}).pretty()
+
 // find all movies that have a synopsis that contains the word "gold" and "dragon"
-// Delete Documents
+db.movies.find({ $text: { $search: 'dwarves hobbit'}}).pretty()
+
+///////////////////////// Delete Documents //////////////////////////////
 // delete the movie "Pee Wee Herman's Big Adventure"
 // delete the movie "Avatar"
-// Relationships
+db.movies.remove({"_id" : ObjectId("5e714ff336d21e18cee07938")})
+
+/////////////// Relationships///////////////
 // Insert the following documents into a users collection
         
 
@@ -148,7 +165,7 @@ const { Db, ObjectId } = require("mongodb");
 //             last_name : "Smith"
         
 
-    
+db.users.insert({'username' : 'SallySmith', 'first_name': 'Sally', 'last_name':'Smith'})
         
 
 //             username : JimmyHagen
@@ -156,7 +173,7 @@ const { Db, ObjectId } = require("mongodb");
 //                 first : "Jimmy"
 //                 last : "Hagen"
         
-
+db.users.insert({'username':'JimmyHagen', 'full_name':{'first':'Jimmy', 'last':'Hagen'}})
     
 // Insert the following documents into a posts collection
         
@@ -165,41 +182,31 @@ const { Db, ObjectId } = require("mongodb");
 //             title : Passes out at party
 //             body : Wakes up early and cleans house
         
-
-    
-        
+db.posts.insert({'username':'SallySmith', 'title':'Passes out at party', 'body':'Wakes up early and cleans house'})    
 
 //             username : SallySmith
 //             title : Buys a House
 //             body : Living in a new neighborhood now
         
-
-    
-        
+db.posts.insert({'username':'SallySmith', 'title':'Reports a bug in your code', 'body':'Sends you a Pull Request'})     
 
 //             username : SallySmith
 //             title : Reports a bug in your code
 //             body : Sends you a Pull Request
         
-
-    
-        
+db.posts.insert({'username':'JimmyHagen','title':'Borrows something', 'body':'Returns it when he is done'})        
 
 //             username : JimmyHagen
 //             title : Borrows something
 //             body : Returns it when he is done
         
-
-    
-        
+db.posts.insert({'username':'JimmyHagen', 'title':'Borrow everything', 'body':'The end'})      
 
 //             username : JimmyHagen
 //             title : Borrows everything
 //             body : The end
         
-
-    
-        
+db.posts.insert({'username':'JimmyHagen','title':'Forks you repo on github', 'body':'Sets to private'})    
 
 //             username : JimmyHagen
 //             title : Forks your repo on github
@@ -218,17 +225,17 @@ const { Db, ObjectId } = require("mongodb");
     
 // where [post_obj_id] is the ObjectId of the posts document: "Borrows something"
 
-        
+db.comments.insert({'username':'SallySmith', 'comment':'Hope you got a good deal!','post': ObjectId("5e725e9e51284147f44ec60a")})        
 
 //             username : SallySmith
 //             comment : What's mine is yours!
 //             post : [post_obj_id]
         
-
+db.comments.insert({ 'username':'SallySmith', 'comment': "Don't violate the licensing agreement!", 'post':ObjectId("5e725f1251284147f44ec60c")})
     
 // where [post_obj_id] is the ObjectId of the posts document: "Borrows everything"
 
-        
+db.comments.insert({'username':'JimmyHagen', 'comment':"It still isn't clean", 'post': ObjectId("5e725da751284147f44ec607")})        
 
 //             username : SallySmith
 //             comment : Don't violate the licensing agreement!
@@ -237,7 +244,7 @@ const { Db, ObjectId } = require("mongodb");
 
     
 // where [post_obj_id] is the ObjectId of the posts document: "Forks your repo on github"
-
+db.comments.insert({ 'username':'SallySmith', 'comment': "Don't violate the licensing agreement!", 'post':ObjectId("5e725f1251284147f44ec60c")})
         
 
 //             username : JimmyHagen
@@ -247,7 +254,7 @@ const { Db, ObjectId } = require("mongodb");
 
     
 // where [post_obj_id] is the ObjectId of the posts document: "Passes out at party"
-
+db.comments.insert({'username':'JimmyHagen', 'comment':"It still isn't clean", 'post': ObjectId("5e725da751284147f44ec607")})
         
 
 //             username : JimmyHagen
@@ -257,8 +264,9 @@ const { Db, ObjectId } = require("mongodb");
 
     
 // where [post_obj_id] is the ObjectId of the posts document: "Reports a bug in your code"
+db.comments.insert({'username':'JimmyHagen', 'comment': 'Denied you PR cause I found a hack', 'post': ObjectId("5e725e5551284147f44ec609")})
 
-// Querying related collections
+////////////////// Querying related collections////////////////
 
 // find all users
 db.users.find().pretty()
